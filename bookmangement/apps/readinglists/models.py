f537e6c3a33ad55733289846fc55103706cb73fc
jasirpanghat <jazjasir7@gmail.com>
2025-08-07 19:57:19 +0530

    feat: create models for ReadingList section

--- bookmangement/apps/readinglists/models.py
+++ bookmangement/apps/readinglists/models.py
@@ -1,2 +1,21 @@
+from users.models import CustomeUser
+from books.models import Books
+from django.db import models
+
+# Create your views here.
+class ReadingList(models.Model):
+    user = models.ForeignKey(CustomeUser, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class ReadingListBook(models.Model):
+    reading_list = models.ForeignKey(ReadingList, on_delete=models.CASCADE)
+    book = models.ForeignKey(Books, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+
 
     
